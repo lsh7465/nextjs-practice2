170ba5c8529df143e4e35591369d40321f43bec1
lsh7465 <lsh746527@gmail.com>
2024-04-02 17:52:19 +0900

    상세페이지 - useRouter

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -6,6 +6,8 @@ export default async function Detail(props) {
   let result = await db
     .collection("post")
     .findOne({ _id: new ObjectId(props.params.id) });
+  // props.params.id는 [id].js와 props.params.id로 접근가능
+  // /detail/123인 경우, props.params.id는"123"과 같은 문자열 값을 가짐
   console.log(props.params.id);
   return (
     <div>
