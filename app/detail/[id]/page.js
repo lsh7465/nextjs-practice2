2e1fd8796d7db63da75f6c29fcf2f50957c4084e
lsh7465 <lsh746527@gmail.com>
2024-04-02 17:23:07 +0900

    상세페이지 - Dynamic route

---
+++ app/detail/[id]/page.js
@@ -0,0 +1,17 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+export default async function Detail(props) {
+  const db = (await connectDB).db("forum");
+  let result = await db
+    .collection("post")
+    .findOne({ _id: new ObjectId(props.params.id) });
+  console.log(props.params.id);
+  return (
+    <div>
+      <h4>상세페이지</h4>
+      <h4>{result.title}</h4>
+      <p>{result.content}</p>
+    </div>
+  );
+}
--- app/list/page.js
+++ app/list/page.js
@@ -1,4 +1,5 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function List() {
   const db = (await connectDB).db("forum");
@@ -9,9 +10,15 @@ export default async function List() {
     <div className="list-bg">
       {result.map((a, i) => {
         return (
-          <div className="list-item" key={i}>
-            <h4>{result[i].title}</h4>
-            <p>{result[i].content}</p>
+          <div className="list-item">
+            <Link
+              href={"/detail/" + result[i]._id}
+              className="list-itemLink"
+              key={i}
+            >
+              <h4>{a.title}</h4>
+            </Link>
+            <p>1월 1일</p>
           </div>
         );
       })}
